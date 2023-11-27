140548c03b45a442689e0a103adc0ba2648b0c9f
spotifyfordarshu <151853726+spotifyfordarshu@users.noreply.github.com>
2023-11-28 00:37:40 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -137,7 +137,7 @@ async def start_command(client: Client, message: Message):
             link = await get_shortlink(SHORTLINK_URL, SHORTLINK_API, f'https://telegram.dog/{client.username}?start=verify_{token}')
             btn = [
                 [InlineKeyboardButton("Click here", url=link)],
-                [InlineKeyboardButton('🗳 Tutorial 🗳', url="www.google.com")]
+                [InlineKeyboardButton('🗳 Tutorial 🗳', url="https://t.me/The_How_To_Open/8")]
             ]
             await message.reply(f"Your Ads token is expired, refresh your token and try again.\n\nToken Timeout: {get_exp_time(VERIFY_EXPIRE)}\n\nWhat is the token?\n\nThis is an ads token. If you pass 1 ad, you can use the bot for {get_exp_time(VERIFY_EXPIRE)} after passing the ad.", reply_markup=InlineKeyboardMarkup(btn), protect_content=True, quote=True)
 
